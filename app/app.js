64926d884d42c8899c053e5c7ceb1409723a6623
longplayer <bartaber@hotmail.com>
2021-05-31 16:36:28 +0200

    created app folder

---
+++ app/app.js
@@ -0,0 +1,34 @@
+const { app, BrowserWindow } = require("electron") // eslint-disable-line
+// const puppeteer = require("puppeteer-core");
+
+const path = require("path")
+const url = require("url")
+let win
+
+function createWindow() {
+  win = new BrowserWindow({
+    width: 1000,
+    height: 600,
+    show: false,
+  })
+
+  win.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "index.html"),
+      protocol: "file:",
+      slashes: true,
+    })
+  )
+
+  win.once("ready-to-show", () => {
+    win.show()
+  })
+
+  win.on("closed", () => {
+    app.quit()
+  })
+}
+
+app.on("ready", () => {
+  createWindow()
+})
